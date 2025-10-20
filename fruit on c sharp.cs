08f338c23444042c9d7062d5eca19f14f301b90a
RusLineSoft <151221747+RusLineSoft@users.noreply.github.com>
2025-10-20 10:24:18 +0500

    Add file with ASCII fruit

---
+++ fruit on c sharp.cs
@@ -0,0 +1,98 @@
+using System;
+
+
+class Program {
+    static void Main() {
+        Console.Write("[BANANA#] Write \"grapes\" or \"banan\", please: ");
+        string banan = Console.ReadLine();
+        if (banan == "grapes") {
+            Console.WriteLine("[BANANA#] Start dra\\/\\/ing");
+            Console.WriteLine("                            __");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                           /  `~-,");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                          /     /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                 ___     {     /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("              ,-'   `-.  ;    :");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("             /         \\/    ;'");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("            !           |   |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("          ,-|        ,--+-. \\,--.");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         /  {       /      \\/    \\");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         |   \\     /        ;     )");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         |   /`-.__|        |    _!_");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         \\  (      |        )  ,'   `.");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("          `-|       \\      / )/       \\");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("           _(        `-..-'  !         |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         ,'  \\  ___  /,-\"\"\"-.|         |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        /     ,'   `./       \\        /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        |    /       \\        )`-._,-'");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        (    |       |        |    |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         \\   \\       /        ).  /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         /`-._:-._,-'\\       /  \\'");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        (        ,'   `-._,-'    |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        (       /        \\       |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("        \\     (          )     /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("          `+.__|          |__,-';");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("           |   (          )     |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("           \\  / \\        /      |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("            `-|  `-.__,-'      /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("              \\       /   |");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("               `-._.-'    )");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                 \\       /");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                  `-._,-'");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("   ||\n   ||\n   ||\n   ||\n   ||\n   ||\n   \\/\n \n[BANANA#] Complete");
+            Console.Write("[BANANA#] PRESS ENTER...");
+            Console.ReadLine();
+            Console.WriteLine("[BANANA#] Good bye, bro");
+        } else if (banan == "banan") {
+            Console.WriteLine("[BANANA#] Start dra\\/\\/ing");
+            Console.WriteLine(" /`-.__                                 /\\");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine(" `. .  ~~--..__                   __..-' ,'");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("   `.`-.._     ~~---...___...---~~  _,~,/");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("     `-._ ~--..__             __..-~ _-~");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("         ~~-..__ ~~--.....--~~   _.-~");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("                ~~--...___...--~~");
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("   ||\n   ||\n   ||\n   ||\n   ||\n   ||\n   \\/\n \n[BANANA#] Complete");
+            Console.Write("[BANANA#] PRESS ENTER...");
+            Console.ReadLine();
+            Console.WriteLine("[BANANA#] Good bye, bro");
+        } else {
+            Console.WriteLine("You're " + banan);
+        }
+    }
+}
